4451690863e1cd8dcc0ad84e1382ac70b6ecd073
Vedansh217k <vedanshkumar217@gmail.com>
2025-01-02 19:18:10 +0530

    Scopes

---
+++ Samples.py/Scopes.py
@@ -0,0 +1,43 @@
+username = "Chaiaurcode"
+
+def func():
+    username = "chai"
+    print(username)
+print(username)
+func()
+
+
+# x= 99
+# def func2(y):
+#     z = x+y
+#     return z
+# result = func2(1)
+# print(result)
+
+# def func3():
+    
+#     x=88
+# func3()
+# print(x)
+
+
+# def f1():
+#     x = 88
+#     def f2():
+#         print(x)
+#     return f2 
+
+# r = f1()
+
+# r()
+
+
+# def chaicoder(num):
+#     def actual(x):
+#         return x** num
+#     return actual   
+    
+# f = chaicoder(2)
+# g = chaicoder(3)
+# print(f(2))
+# print(g(5))
